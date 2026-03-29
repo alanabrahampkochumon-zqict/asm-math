1b9189b092bf0b049bfb4b685027fde3ab2a85c1
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-03-29 16:37:26 +0530

    refactor: remove test add function

--- asm-math/src/scalars/add.asm
+++ asm-math/src/scalars/add.asm
@@ -22,19 +22,6 @@ ENDM
 ;                     CODE SEGMENT
 ;=========================================================
 .code
-
-PUBLIC _asm_scalar_add                                                                                    ; Expose the functions so that C++ can use them
-                                                                                                          
-XOR RCX, RCX                                                                                              ; Clear out RCX
-XOR RDX, RDX                                                                                              ; Clear out RDX
-                                                                                                          
-_asm_scalar_add PROC                                                                                      
-	ADD RCX, RDX                                                                                          ; Adding RCX and RDX and store the result in RCX
-	MOV RAX, RCX                                                                                          ; Move result into RAX before returning
-	RET
-
-_asm_scalar_add ENDP
-
 GENERATE_ADD_PROC_INT _asm_scalar_add_8, CL, DL, AL                                                       ; Macro invocation for 8-bit integrals.
 GENERATE_ADD_PROC_INT _asm_scalar_add_16, CX, DX, AX                                                      ; Macro invocation for 16-bit integrals.
 GENERATE_ADD_PROC_INT _asm_scalar_add_32, ECX, EDX, EAX                                                   ; Macro invocation for 32-bit integrals.
