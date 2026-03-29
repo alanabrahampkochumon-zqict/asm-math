8759ff19cdb30d5724af0aaa8a76e1e81fa566de
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-03-29 13:16:36 +0530

    fix: Math.h name conflict

---
+++ asm-math/src/scalars/add.asm
@@ -0,0 +1,15 @@
+; Sample code snippet
+
+.data
+
+.code
+
+PUBLIC asm_add_integers ; Expose the functions so that C++ can use them
+
+
+asm_add_integers PROC
+
+asm_add_integers ENDP
+
+
+END
---
+++ playground/src/main.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file main.cpp
+ * @author Alan Abraham P Kochumon
+ * @date Created on: March 29, 2026
+ *
+ * @brief Main entry point of playground.
+ * 
+ * @copyright Copyright (c) 2026 Alan Abraham P Kochumon
+ */
+
+
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	std::cout << "sqrt(4.0) = " << std::sqrt(4.0f) << '\n';
+	std::cout << "1 + 2 = " << (1 + 2) << "\n";
+	std::cout << "Playground\n";
+}
+
